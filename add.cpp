415063b079eee5ec6396d0c22d4f4b88b969c464
dhia <dhiaguerfi49@gmail.com>
2024-07-17 16:19:36 +0200

    I've edited this file as Guerfi_Dhia Branche

--- add.cpp
+++ add.cpp
@@ -2,8 +2,11 @@
 using namespace std;
 int main(){
 int x,y;
-cin>>x>>y;
-cout<<x+y;
+cout<<"hello world"<<endl;
+cout<<"Enter tow integers: "
+cin>>x;
+cin>>y;
+cout<<"the sym of them is equal to: "<<x+y;
 return 0;
 }
 
